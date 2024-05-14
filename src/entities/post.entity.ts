fdc41f33887fbbf3e595bbf557e38f03494792de
dinh <dinhtq170601@gmail.com>
2024-05-14 15:41:34 +0700

    update entity category, user, post and categri service

--- src/entities/post.entity.ts
+++ src/entities/post.entity.ts
@@ -1,5 +1,14 @@
 import { Transform } from 'class-transformer';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import User from './user.entity';
+import { Category } from './category.entity';
 
 @Entity()
 class Post {
@@ -18,7 +27,12 @@ class Post {
       return value;
     }
   })
-  public category?: string;
+  @ManyToOne(() => User, (author: User) => author.posts)
+  public author: User;
+
+  @ManyToMany(() => Category, (category: Category) => category.posts)
+  @JoinTable()
+  public categories: Category;
 }
 
 export default Post;
